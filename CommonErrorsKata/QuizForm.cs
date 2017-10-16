b2e1674ba130743d0c3d1a00f60326683f68d300
Jessica Reid-Harris <jrharris620@yahoo.com>
2017-10-16 08:12:41 -0500

    Removed unused directives

--- CommonErrorsKata/QuizForm.cs
+++ CommonErrorsKata/QuizForm.cs
@@ -2,8 +2,6 @@
 using System.IO;
 using System.Linq;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
